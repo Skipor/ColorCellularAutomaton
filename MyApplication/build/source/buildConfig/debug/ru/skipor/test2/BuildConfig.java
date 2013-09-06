13789b45f99c479120adc3929f498975aa185b84
Vladimir Skipor <vladimirskipor@gmail.com>
2013-09-06 22:32:23 +0400

    Initial commit. Not working yet.

---
+++ MyApplication/build/source/buildConfig/debug/ru/skipor/test2/BuildConfig.java
@@ -0,0 +1,7 @@
+/** Automatically generated file. DO NOT MODIFY */
+package ru.skipor.test2;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+
+}
---
+++ MyApplication/src/main/java/ru/skipor/test2/MainActivity.java
@@ -0,0 +1,81 @@
+package ru.skipor.test2;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.View;
+
+import java.util.Random;
+
+
+public class MainActivity extends Activity {
+
+
+    static class myView extends View {
+        final static int COLORS_NUM = 16;
+        final static int INITIAL_COLOR = 0; // < COLORS_NUM
+        final static int HEIGHT = 640;
+        final static int WIDTH = 480;
+        State current;
+        State next;
+
+        final static Paint[] paints = new Paint[COLORS_NUM];
+        final static Random random = new Random();
+
+
+
+        static {
+            for (int i = 0; i < COLORS_NUM; i++) {
+                paints[i] = new Paint();
+                paints[i].setARGB(255, random.nextInt(256), random.nextInt(256), random.nextInt(256));
+
+            }
+        }
+
+        class State {
+            int colors[][];
+
+            State() {
+                colors = new int[myView.HEIGHT][myView.WIDTH];
+                for(int i = )
+            }
+        }
+
+        public myView(Context context) {
+            super(context);
+            current = new State();
+            current = new State();
+        }
+
+        protected void nextState() {
+
+
+        }
+
+
+        @Override
+        public void onDraw(Canvas canvas) {
+            Paint paint = new Paint();
+            paint.setARGB(255, 0, 0, 0);
+            canvas.drawRect(10, 10, 90, 90, paint);
+            invalidate();
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(new myView(this));
+    }
+
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.main, menu);
+//        return true;
+//    }
+
+}
