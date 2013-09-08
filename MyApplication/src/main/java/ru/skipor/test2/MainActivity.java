4d1802e3226bb7ee6a755838ea5e88e638d3e005
Vladimir Skipor <vladimirskipor@gmail.com>
2013-09-08 21:25:31 +0400

    working version. 1-2 fps.

--- MyApplication/src/main/java/ru/skipor/test2/MainActivity.java
+++ MyApplication/src/main/java/ru/skipor/test2/MainActivity.java
@@ -3,6 +3,7 @@ package ru.skipor.test2;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.View;
@@ -15,26 +16,40 @@ public class MainActivity extends Activity {
 
     static class myView extends View {
         final static int COLORS_NUM = 16; //16 is only possible
-        final static int HEIGHT = 640;
-        final static int WIDTH = 480;
-        State current;
-        State next;
+        final static int HEIGHT = 320;
+        final static int WIDTH = 240;
+        final static int RECT_HEIGHT = 3; //pixel
+        final static int RECT_WIDTH = 3;
+
+        final static Rect[][] rectangles = new Rect[HEIGHT][WIDTH];
 
         final static Paint[] paints = new Paint[COLORS_NUM];
         final static Random random = new Random();
+        State current;
+        State next;
 
+        ///
+        boolean firstDraw = true;
+        int count = 0;
+        ///
 
 
         static {
             for (int i = 0; i < COLORS_NUM; i++) {
                 paints[i] = new Paint();
                 paints[i].setARGB(255, random.nextInt(256), random.nextInt(256), random.nextInt(256));
-
             }
+            for (int i = 0; i < HEIGHT; i++) {
+                for (int j = 0; j < WIDTH; j++) {
+                    rectangles[i][j] = new Rect(j * RECT_WIDTH, i * RECT_HEIGHT, (j + 1) * (RECT_WIDTH), (i + 1) * (RECT_HEIGHT ));
+                }
+            }
+
+
         }
 
         class State {
-            int colors[][];
+            public int colors[][];
 
             State() {
                 colors = new int[myView.HEIGHT][myView.WIDTH];
@@ -52,21 +67,88 @@ public class MainActivity extends Activity {
         public myView(Context context) {
             super(context);
             current = new State();
-            current = new State();
-        }
-
-        protected void nextState() {
-
+            current.setRandom();
+            next = new State();
 
         }
+
+
 
 
         @Override
         public void onDraw(Canvas canvas) {
-            Paint paint = new Paint();
-            paint.setARGB(255, 0, 0, 0);
-            canvas.drawRect(10, 10, 90, 90, paint);
-            invalidate();
+//            if (firstDraw){
+//                firstDraw = false;
+            current.setRandom();
+                for (int i = 0; i < HEIGHT; i++) {
+                    for (int j = 0; j < WIDTH; j++) {
+                        canvas.drawRect(rectangles[i][j], paints[current.colors[i][j]]);
+                    }
+                }
+//            }
+
+////
+////            while (count < 1) {
+////                ++count;
+//
+//
+//            State swapState;
+//            int nextColor;
+//            boolean changeColor;
+//                for (int i = 1; i < HEIGHT - 1; i++) {// most part change
+//                    for (int j = 1; j < WIDTH - 1; j++) {
+//                        changeColor = false;
+//                        nextColor = ((current.colors[i][j] + 1) & 15);
+//                        changeColor |= (current.colors[i - 1][j - 1] == nextColor);
+//                        changeColor |= (current.colors[i - 1][j] == nextColor);
+//                        changeColor |= (current.colors[i - 1][j + 1] == nextColor);
+//                        changeColor |= (current.colors[i][j - 1] == nextColor);
+//                        changeColor |= (current.colors[i][j + 1] == nextColor);
+//                        changeColor |= (current.colors[i + 1][j - 1] == nextColor);
+//                        changeColor |= (current.colors[i + 1][j] == nextColor);
+//                        changeColor |= (current.colors[i + 1][j + 1] == nextColor);
+////                        next.colors[i][j] = changeColor ? nextColor : current.colors[i][j];
+//                        if (changeColor) {
+//                            canvas.drawRect(rectangles[i][j], paints[nextColor]);
+//                            next.colors[i][j] = nextColor;
+//                        } else {
+//                            next.colors[i][j] = current.colors[i][j];
+//                        }
+//
+//                    }
+//                }
+////                int j = 0;
+////                for (int i = 1; i < HEIGHT - 1; i++) {
+////                    changeColor = false;
+////                    nextColor = ((current.colors[i][j] + 1) & 15);
+////                    changeColor |= (current.colors[i - 1][WIDTH - 1] == nextColor);
+////                    changeColor |= (current.colors[i - 1][j] == nextColor);
+////                    changeColor |= (current.colors[i - 1][j + 1] == nextColor);
+////                    changeColor |= (current.colors[i][WIDTH - 1] == nextColor);
+////                    changeColor |= (current.colors[i][j + 1] == nextColor);
+////                    changeColor |= (current.colors[i + 1][WIDTH - 1] == nextColor);
+////                    changeColor |= (current.colors[i + 1][j] == nextColor);
+////                    changeColor |= (current.colors[i + 1][j + 1] == nextColor);
+//////                        next.colors[i][j] = changeColor ? nextColor : current.colors[i][j];
+////                    if (changeColor) {
+////                        canvas.drawRect(rectangles[i][j], paints[nextColor]);
+////                        next.colors[i][j] = nextColor;
+////                    } else {
+////                        next.colors[i][j] = current.colors[i][j];
+////                    }
+////                }
+////
+////                int j =
+////
+//                swapState = current;
+//                current = next;
+//                next = swapState;
+////            }
+
+if (count < 100){
+invalidate();
+    count++;
+}
         }
     }
 
